76e7fe816badd0e019dbce28b47f9a257a869f3a
FCL00 <flagahit00@gmail.com>
2024-11-29 22:06:09 +0800

    add game controls

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,53 @@ import { useState } from "react";
 import Header from "@/components/Header"
 import Player from "@/components/Player"
 import GameBoard from "@/components/GameBoard"
+import Log from "@/components/Log";
+import GameOver from "@/components/GameOver";
+import {deriveActivePlayer, deriveGameBoard, deriveWinner} from "@/util/controls"
 
 function App() {
-  const [activePlayer, setActivePlayer] = useState("X");
+  // game source
+  const [gameTurns, setGameTurn] = useState([]);
 
-  const handleActivePlayer = () => {
-    setActivePlayer((currentPlayer) => (currentPlayer === "X" ? "O" : "X"));
+  const [players, setPlayer] = useState({
+    "X": "Player 1",
+    "O": "Player 2",
+  });
+  // active player
+  const activePlayer = deriveActivePlayer(gameTurns);
+  // gameboard
+  const gameBoard = deriveGameBoard(gameTurns)
+  // game winner
+  const winner = deriveWinner(gameBoard, players)
+  // In tic-tac-toe the maximum move each player have is 9 so we can use it to check if theres a draw
+  const hasDraw = gameTurns.length === 9 && !winner;
+
+  const handleSelectSquare = (rowIndex, colIndex) => {
+    
+    setGameTurn((prevTurns) => {
+      const currentPlayer = deriveActivePlayer(prevTurns);
+
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+        return updatedTurns;
+    });
   };
+
+  const handleRematch = () => {
+    setGameTurn([]);
+  }
+
+
+  const handlePlayerNameChange = (symbol, newName) =>{
+    setPlayer((prevPlayers) => {
+      return {
+        ...prevPlayers,
+        [symbol] : newName,
+      }
+    })
+  }
 
   return (
     <>
@@ -16,15 +56,16 @@ function App() {
       <main >
           <div id="game-container">
             <ol id="players" className="highlight-player">
-              <Player name="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
-              <Player name="Player 2" symbol="O" isActive={activePlayer === 'O'}/>
+              <Player name="Player 1" symbol="X" isActive={activePlayer === 'X'} onChangeName={handlePlayerNameChange}/>
+              <Player name="Player 2" symbol="O" isActive={activePlayer === 'O'} onChangeName={handlePlayerNameChange}/>
             </ol>
+            {(winner || hasDraw) && <GameOver winner={winner} restart={handleRematch}/>}
             <GameBoard
-              currentPlayer={handleActivePlayer} 
-              activePlayerSymbol={activePlayer} 
+              onSelectSquare={handleSelectSquare} 
+              board={gameBoard}
             />
           </div>
-          {/* Logs */}
+          <Log turns={gameTurns}/>
       </main>
     </>
   )
